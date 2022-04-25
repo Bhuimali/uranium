3ef9d90842dc6224a1f3c2a125db7def3e53cd4d
Ananta Bhuimali <anantaimo@gmail.com>
2022-04-25 09:42:34 +0530

    22 April Assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,70 +1,112 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-
-const createUser = async function (req, res) {
-  let getUserData = req.body;
-  let showData = await User.create(getUserData);
-  res.send({ status: true, data: showData });
+const createUser = async(req, res) => {
+  try{
+    let getUserData = req.body;
+    if(Object.keys(getUserData).length == 0) return res.status(400).send({status: false, msg: 'User details is required to create account'})
+    let showData = await User.create(getUserData);
+    res.status(201).send({ status: true, data: showData });
+  } catch(err){
+    res.status(500).send({status: false, msg: err.message})
+  }
 };
 
-const loginUser = async function (req, res) {
-  let email = req.body.email;
-  let password = req.body.password;
+const loginUser = async(req, res) => {
+  try {
+    let email = req.body.email;
+    let password = req.body.password;
+    if(!email && !password) return res.status(400).send({status: false, msg: 'Email and password is required!'});
 
-  let user = await User.findOne({ emailId: email, password: password });
-  if (!user){
-    return res.send({status: false, msg: "Email Id or password is not correct",});
+    let user = await User.findOne({ emailId: email, password: password });
+    if (!user){
+      return res.status(401).send({status: false, msg: "Email Id or password is not correct",});
+    }else if(user.isDeleted == true){
+      return res.status(401).send({status: false, msg: "Logging failed! Account deleted",});
+    }
 
+    let token = jwt.sign({ userId: user._id.toString()}, "FunctionUp-Uranium");
+    res.setHeader("x-auth-token", token);
+    res.send({ status: true, data: 'Logged in successfully' });
+  } catch (err) {
+    res.status(500).send({status: false, msg: err.message});
   }
-
-  let token = jwt.sign({ userId: user._id.toString()}, "FunctionUp-Uranium");
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
 };
 
-const getUserData = async function (req, res) {
-  let userId = req.params.userId;
 
-  let userData = await User.findById(userId);
-  if (!userData){
-    return res.send({ status: false, msg: "No such user exists" });
+
+
+
+const getUserData = async(req, res) => {
+  try {
+    let userId = req.params.userId;
+    if(!userId) return res.status(400).send({status: false, msg: 'UserId is required'});
+
+    let userData = await User.findById(userId);
+    if (!userData){
+      return res.status(404).send({ status: false, msg: "No such user exists" });
+    }else if(userData.isDeleted == true){
+      return res.status(401).send({status: false, msg: "Users data has been deleted",});
+    }
+
+    res.status(200).send({ status: true, data: userData });
+  }catch (err){
+    res.status(500).send({status: false, msg: err.message});
   }
-
-  res.send({ status: true, data: userData });
 };
 
-const updateUser = async function (req, res) {
-  let userId = req.params.userId;
-  let user = await User.findById(userId);
-  
-  if (!user) {
-    return res.send("No such user exists");
-  }
+const updateUser = async(req, res) => {
+  try {
+    let userId = req.params.userId;
+    if(!userId) return res.status(400).send({status: false, msg: 'UserId is required'});
 
-  let userData = req.body;
-  let updatedUser = await User.findOneAndUpdate(
-    { _id: userId }, 
-    { $set: userData},
-    { new: true},
-    );
-  res.send({ status: true, data: updatedUser });
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send({status: false, msg: "No such user exists"});
+    }else if(user.isDeleted == true){
+      return res.status(401).send({status: false, msg: "Sorry, cannot update data of deleted user",});
+    }
+
+    let userData = req.body;
+    if(Object.keys(userData).length == 0) {
+      return res.status(400).send({status: false, msg: 'User details is required to update user account'});
+    } else if(userData.hasOwnProperty('password') || userData.hasOwnProperty('isDeleted')){
+      return res.status(403).send({status: false, msg: 'Cannot update the selected field'})
+    }
+
+    let updatedUser = await User.findOneAndUpdate(
+      { _id: userId }, 
+      { $set: userData},
+      { new: true},
+      );
+    res.status(200).send({ status: true, data: updatedUser });
+  }catch (err){
+    res.status(500).send({status: false, msg: err.message});
+  }
 };
 
-const deleteUser = async function(req, res){
-  let userId = req.params.userId;
-  let getUserId = await User.findById(userId)
+const deleteUser = async(req, res) => {
+  try {
+    let userId = req.params.userId;
+    if(!userId) return res.status(400).send({status: false, msg: 'UserId is required'});
 
-  if(!getUserId) {
-    return res.send("No such User exits");
+    let getUserId = await User.findById(userId);
+    if (!getUserId) {
+      return res.status(404).send("No such user exists");
+    }else if(getUserId.isDeleted == true){
+      return res.status(404).send({status: false, msg: "User data is already deleted"});
+    }
+
+    let user = await User.findOneAndUpdate(
+      {_id: userId},
+      { $set: {isDeleted: true}},
+      { new: true}
+    )
+
+    res.status(200).send({status: true, msg: "Account has been deleted"});
+  }catch (err){
+    res.status(500).send({status: false, msg: err.message});
   }
-  let user = await User.findOneAndUpdate(
-    {_id: userId},
-    { $set: {isDeleted: true}},
-    { new: true}
-  )
-
-  res.send({status: true, data: user});
 }
 
 module.exports.createUser = createUser;
